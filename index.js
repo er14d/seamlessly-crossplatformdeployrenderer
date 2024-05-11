3a820763d7a349bf93c227dffe43c245e92c8b7e
er14d <blueghostpsg@hotmail.com>
2024-05-11 13:26:07 +0700

    test commit

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function buildTree(preorder, inorder) {
-  if (preorder.length === 0 || inorder.length === 0) return null;
-  const rootVal = preorder[0];
-  const root = new TreeNode(rootVal);
-  const index = inorder.indexOf(rootVal);
-  const leftInorder = inorder.slice(0, index);
-  const rightInorder = inorder.slice(index + 1);
-  const leftPreorder = preorder.slice(1, 1 + leftInorder.length);
-  const rightPreorder = preorder.slice(1 + leftInorder.length);
-  root.left = buildTree(leftPreorder, leftInorder);
-  root.right = buildTree(rightPreorder, rightInorder);
-  return root;
+function exist(board, word) {
+  const rows = board.length;
+  const cols = board[0].length;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (dfs(board, i, j, word, 0)) return true;
+    }
+  }
+  return false;
+  function dfs(board, i, j, word, index) {
+    if (index === word.length) return true;
+    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
+      return false;
+    const temp = board[i][j];
+    board[i][j] = "#";
+    const found =
+      dfs(board, i + 1, j, word, index + 1) ||
+      dfs(board, i - 1, j, word, index + 1) ||
+      dfs(board, i, j + 1, word, index + 1) ||
+      dfs(board, i, j - 1, word, index + 1);
+    board[i][j] = temp;
+    return found;
+  }
 }
