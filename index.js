8c70a9e26f4e670d33cc5ac8da40f163b7283982
er14d <blueghostpsg@hotmail.com>
2024-05-17 09:17:34 +0700

    start

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-function threeSum(nums) {
-  nums.sort((a, b) => a - b);
-  const result = [];
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
-      let low = i + 1;
-      let high = nums.length - 1;
-      let sum = 0 - nums[i];
-      while (low < high) {
-        if (nums[low] + nums[high] === sum) {
-          result.push([nums[i], nums[low], nums[high]]);
-          while (low < high && nums[low] === nums[low + 1]) low++;
-          while (low < high && nums[high] === nums[high - 1]) high--;
-          low++;
-          high--;
-        } else if (nums[low] + nums[high] < sum) low++;
-        else high--;
-      }
+function findCircleNum(M) {
+  if (!M || M.length === 0) return 0;
+  const visited = new Array(M.length).fill(false);
+  let count = 0;
+  for (let i = 0; i < M.length; i++) {
+    if (!visited[i]) {
+      dfs(M, i, visited);
+      count++;
     }
   }
-  return result;
+  return count;
+}
+function dfs(M, i, visited) {
+  visited[i] = true;
+  for (let j = 0; j < M.length; j++) {
+    if (M[i][j] === 1 && !visited[j]) {
+      dfs(M, j, visited);
+    }
+  }
 }
